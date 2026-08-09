888c343fd51d7a8b14c1529017f11f83adf9214d
asatullayev-diyorbek <asatullayevdiyorbek3@gmail.com>
2026-08-10 01:34:00 +0500

    Hide site chrome and add arena battle flow

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -5,14 +5,15 @@ import Footer from "../components/Footer"
 export default function MainLayout() {
   const location = useLocation()
   const isQuizRun = /^\/tests\/[^/]+$/.test(location.pathname)
+  const isArena = /^\/arena\/[^/]+(?:\/play)?$/.test(location.pathname)
 
   return (
     <div className="min-h-screen flex flex-col">
-      {!isQuizRun && <Navbar />}
+      {!isQuizRun && !isArena && <Navbar />}
       <main className="flex-1">
         <Outlet />
       </main>
-      {!isQuizRun && <Footer />}
+      {!isQuizRun && !isArena && <Footer />}
     </div>
   )
 }
